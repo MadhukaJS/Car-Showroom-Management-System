8123e0692b568e3f27e956eedf63a9deb54a9af3
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-10-21 11:43:16 +0530

    update ui making

--- car-showroom/src/App.js
+++ car-showroom/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
+import AdminDashboard from './components/admin/AdminDashboard';
+import OwnerDashboard from './components/owner/OwnerDashboard';
+import VisitorDashboard from './components/visitor/VisitorDashboard';
+// import AdminSlidebar from './components/admin/AdminSidebar';
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="adminDashoboard" element={<AdminDashboard />} />
+        <Route path="visitordashboard" element={<VisitorDashboard />} />
+        {/* <Route path="adminsidebar" element={<AdminSlidebar/>}/> */}
       </Routes>
     </Router>
 
---
+++ car-showroom/src/components/admin/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './AdminSidebar'
+
+export default function AdminDashboard() {
+  return (
+
+    <div className="d-flex flex-column flex-lg-row h-lg-full bg-surface-secondary">
+          <Sidebar />
+    
+      
+    </div>
+  )
+}
