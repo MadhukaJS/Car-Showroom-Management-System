d103027f448d085a3edecaadb2660d4a1eba6646
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-10-25 15:57:39 +0530

    admin dashboard finished.

--- car-showroom/src/components/admin/AdminDashboard.jsx
+++ car-showroom/src/components/admin/AdminDashboard.jsx
@@ -1,19 +1,78 @@
-import React from 'react'
-import Sidebar from './AdminSidebar'
-import Header from './AdminHeader'
+import React from "react";
+import Sidebar from "./AdminSidebar";
+import './adminstyle.css';
+
 
 export default function AdminDashboard() {
   return (
+    <div id="wrapper">
+      <Sidebar />
 
-    <div className="h-screen flex-grow-1 overflow-y-lg-auto">
-            <Header/>
-            
-    <div className="d-flex flex-column flex-lg-row h-lg-full bg-surface-secondary">
-          <Sidebar />
+      {/* Content Wrapper */}
+      <div id="content-wrapper" className="d-flex flex-column">
+        {/* Main Content */}
+        <div id="content">
+          {/* Topbar */}
+          <nav className="navbar navbar-expand navbar-light  topbar mb-4 static-top shadow">
+            {/* Sidebar Toggle (Topbar) */}
+            <button
+              id="sidebarToggleTop"
+              className="btn btn-link d-md-none rounded-circle mr-3"
+            >
+              <i className="fa fa-bars"></i>
+            </button>
+
+
+            {/* Topbar */}
+            <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+              <div className="input-group">
+                <h3></h3>
+              </div>
+            </form>
+          </nav>
+
+{/* -------------------------------------------------------------------------------------------------- */}
+
+          {/* Main Content */}
+          <div className="container-fluid">
+            <h1 className="h3 mb-4 text-gray-800">Admin Dashboard</h1>
+            {/* Add your page content here */}
+            <div className="d-flex justify-content-center align-items-center p-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Add Users</h5>
+                            <p className="card-text">Add new users to your system.</p>
+                            <a href="/add-users" className="btn ">Go to Add Users</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">View Users</h5>
+                            <p className="card-text">View and manage existing users.</p>
+                            <a href="/view-users" className="btn ">Go to View Users</a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
           </div>
+        </div>
+
+
+{/* -------------------------------------------------------------------------------------------- */}
+        {/* Footer */}
+        <footer className="sticky-footer ">
+          <div className="container my-auto">
+            <div className="copyright text-center my-auto">
+              <span>MPS motor Traders &copy;2023</span>
+            </div>
+          </div>
+        </footer>
+      </div>
+
+      
     </div>
-
-
-
-  )
+  );
 }
--- car-showroom/src/components/admin/AdminSidebar.jsx
+++ car-showroom/src/components/admin/AdminSidebar.jsx
@@ -1,18 +1,80 @@
-// Sidebar.js
-import React from 'react';
-import './adminstyle.css'; // You will create this CSS file
+import React from "react";
+import './adminstyle.css';
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      {/* Sidebar */}
+      <ul
+        className="navbar-nav sidebar sidebar-dark accordion"
+        id="accordionSidebar"
+      >
+        {/* Sidebar - Brand */}
+        <a
+          className="sidebar-brand d-flex align-items-center justify-content-center"
+          href="index.html"
+        >
+          <div className="sidebar-brand-icon rotate-n-15">
+            <i className="fas fa-laugh-wink"></i>
+          </div>
+          <div className="sidebar-brand-text mx-3">
+            MPS Motors 
+          </div>
+        </a>
+
+        <hr className="sidebar-divider my-0" />
+
+        <li className="nav-item">
+          <a className="nav-link" href="index.html">
+            <i className="fas fa-fw fa-tachometer-alt"></i>
+            <span>Homepage</span>
+          </a>
+        </li>
+
+        <hr className="sidebar-divider" />
+
+        <div className="sidebar-heading"></div>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <i className="fas fa-fw fa-cog"></i>
+            <span>View owners</span>
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <i className="fas fa-fw fa-wrench"></i>
+            <span>View visitors</span>
+          </a>
+        </li>
+
+        <hr className="sidebar-divider" />
+
+        <div className="sidebar-heading"></div>
+
+        
+
+        <li className="nav-item">
+          <a className="nav-link" href="charts.html">
+            <i className="fas fa-fw fa-chart-area"></i>
+            <span>Admin Profile</span>
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="tables.html">
+            <i className="fas fa-fw fa-table"></i>
+            <span>Logout</span>
+          </a>
+        </li>
+
+        <hr className="sidebar-divider d-none d-md-block" />
+
+        
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
