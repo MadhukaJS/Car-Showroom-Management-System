26e40868a450607aa6278d2fede73c7e4ae3278e
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2024-01-05 23:30:13 +0530

    update

--- car-showroom/src/components/Login.jsx
+++ car-showroom/src/components/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
         [name]:value,
     });
   };
-// jwt token not working
+//working but not performrd
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
