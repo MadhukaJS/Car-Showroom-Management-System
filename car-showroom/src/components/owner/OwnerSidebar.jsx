6f93745e61f88f5a90311f878a88933e3f3401e0
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2024-01-10 23:12:14 +0530

    update format

--- car-showroom/src/components/owner/OwnerSidebar.jsx
+++ car-showroom/src/components/owner/OwnerSidebar.jsx
@@ -24,12 +24,7 @@ const Sidebar = () => {
 
         <hr className="sidebar-divider my-0" />
 
-        {/* <li className="nav-item">
-          <a className="nav-link" href="index.html">
-            <i className="fas fa-fw fa-tachometer-alt"></i>
-            <span>Homepage</span>
-          </a>
-        </li> */}
+      
 
         <hr className="sidebar-divider" />
 
