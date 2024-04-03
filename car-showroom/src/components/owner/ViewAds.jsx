7e885a414cb5d6c8c608779c6c917a37ceaa0ed7
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2024-04-03 23:35:08 +0530

    update details

--- car-showroom/src/components/owner/ViewAds.jsx
+++ car-showroom/src/components/owner/ViewAds.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-
+//VIEW booked vehicles
 function ViewAds() {
   const [advertisement, setAdvertisement] = useState(null);
 
